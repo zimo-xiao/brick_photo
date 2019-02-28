8973c8d3cd926c5185a665f03e8dc9f43f0a6ef8
zimo-xiao <xiaozimo@zuggr.com>
2019-02-28 22:23:20 +0800

    debug

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+// debug
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
