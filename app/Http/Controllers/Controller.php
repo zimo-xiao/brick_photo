a44e35c5f692cd4f2ebf34bc448493e7fa8eb0dc
zimo-xiao <xiaozimo@zuggr.com>
2019-03-15 13:53:31 +0800

    添加redis提速

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,15 +16,21 @@ class Controller extends BaseController
         $token = $request->session()->get('token');
         if ($token != null) {
             try {
-                $out = Curl::to($request->root().'/auth')
-                    ->withHeader('Authorization: Bearer '.$token)
-                    ->asJson()
-                    ->get();
+                if (\Cache::store('redis')->has('user_info_'.$token)) {
+                    $out = json_decode(\Cache::store('redis')->get('user_info_'.$token));
+                } else {
+                    $out = Curl::to($request->root().'/auth')
+                        ->withHeader('Authorization: Bearer '.$token)
+                        ->asJson()
+                        ->get();
+                    \Cache::store('redis')->put(json_encode($out), 'user_info_'.$token, 5);
+                }
                 // HACK: 测试用户是否存在，故意报错
                 $out->permission;
                 return $out;
             } catch (\Exception $e) {
                 $request->session()->forget('token');
+                \Cache::store('redis')->delete('user_info_'.$token);
                 return json_decode(json_encode([
                     'name' => null,
                     'permission' => null,
