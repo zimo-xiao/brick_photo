d551ea425607775499552cc42647fdb23d4d99d9
zimo-xiao <xiaozimo@zuggr.com>
2019-03-29 18:04:55 +0800

    Update delete_box.php

--- resources/views/component/delete_box.php
+++ resources/views/component/delete_box.php
@@ -9,8 +9,9 @@
                     <label for="delete_box_reason">请简单描述删除原因</label>
                 </div>
                 <div class="input-field col s12">
-                    <button onclick="$('#delete_box').hide();redRequest.delete()" class="btn waves-effect waves-light"
-                        style="background-color: #EA5662" type="button" name="action">删除</button>
+                    <button onclick="$('#delete_box').hide();adminRequest.deleteImage()"
+                        class="btn waves-effect waves-light" style="background-color: #EA5662" type="button"
+                        name="action">删除</button>
                 </div>
                 <input type="hidden" value="" id="delete_box_image_id">
             </div>
