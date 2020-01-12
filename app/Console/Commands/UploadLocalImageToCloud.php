f06cf00396e31796d7310415b0bcc1342dfaa399
Zimo Xiao <xiaozimo@zuggr.com>
2020-01-12 23:37:56 +0800

    Update UploadLocalImageToCloud.php

--- app/Console/Commands/UploadLocalImageToCloud.php
+++ app/Console/Commands/UploadLocalImageToCloud.php
@@ -50,7 +50,7 @@ class UploadLocalImageToCloud extends Command
             try {
                 $this->upload($image['path'], $image['file_name'].''.$image['file_format']);
             } catch(\Exception $e) {
-                $this->info('error when processing '.$image['file_name']);
+                $this->info('error when processing '.$image['file_name'].': '.$e->getMessage());
             }
         }
     }
